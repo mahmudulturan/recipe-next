e9936d6a6058087b27a61a3ac013b1dd6c9cd420
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-01-16 18:22:37 +0600

    Basic Setup: Color pallet and font

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,7 @@
-import ingredients from '../../ingredients.json'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold text-center">Recipe app
-      
-      </h1>
+    <main className="">
+      <h1 className="text-4xl font-bold text-center bg-background text-primary">Recipe app</h1>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        text: "#19060f",
+        background: "#fdf8fb",
+        primary: "#d02f83",
+        secondary: "#FFC0CB",
+        accent: "#DAA49A"
       },
     },
   },
